2fb2721ea0d4b69b3047a634c9f735e263247802
xiaobin <xiaobin@xiaobindemac.local>
2016-05-11 03:38:46 -0700

    11

--- APP_DRIVER/APP_DRIVER/StaticRef.h
+++ APP_DRIVER/APP_DRIVER/StaticRef.h
@@ -12,7 +12,7 @@
 //static const NSString *WEBSERVICE_ID = @"app.ahglwl.com";             //正式服务器地址
 //static const NSString *WEBSERVICE_NAME = @"AndroidServiceDriverTest";   //服务器应用名称
 
-static const NSString *WEBSERVICE_ID = @"192.168.1.104";        //服务器地址
+static const NSString *WEBSERVICE_ID = @"192.168.1.115";        //服务器地址
 static const NSString *WEBSERVICE_NAME = @"AndroidServiceDriver";   //服务器应用名称
 
 static const NSString *WEBSERVICE_PORT = @"8083";               //服务器端口
